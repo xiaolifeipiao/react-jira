6a3bfe71d57755428a57aff887e847c3eab1ce12
xiaolifeipiao <1921513506@qq.com>
2022-01-21 16:55:11 +0800

    feat: 添加id-select公共组件抽取实现透传

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+import { type } from 'os';
+
+/**
+@Author: xiaolifeipiao
+@Description: 
+@version: 0.0.0
+@Date: 2022-01-21 16:30:52
+@LastEditTime: 2022-01-21 16:30:52
+@LastEditors: xiaolifeipiao
+@FilePath: \src\types\index.ts
+ */
+export type Raw = string | number;
